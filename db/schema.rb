7fa349f60acd559094d9ba55d6fc20a6daa1a255
Erwann Defoy <ecdefoy1@gmail.com>
2025-03-11 12:21:12 +0100

    seed for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_10_164529) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_11_095533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assignations", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "diagnostic_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["diagnostic_id"], name: "index_assignations_on_diagnostic_id"
+    t.index ["user_id"], name: "index_assignations_on_user_id"
+  end
+
+  create_table "consultations", force: :cascade do |t|
+    t.text "prompt"
+    t.datetime "starts_at"
+    t.bigint "user_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_consultations_on_patient_id"
+    t.index ["user_id"], name: "index_consultations_on_user_id"
+  end
+
+  create_table "diagnostics", force: :cascade do |t|
+    t.text "description"
+    t.string "specialty"
+    t.boolean "selected", default: false
+    t.boolean "specialist_needed", default: false
+    t.bigint "consultation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["consultation_id"], name: "index_diagnostics_on_consultation_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "assignation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["assignation_id"], name: "index_messages_on_assignation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -32,4 +86,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_10_164529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "assignations", "diagnostics"
+  add_foreign_key "assignations", "users"
+  add_foreign_key "consultations", "patients"
+  add_foreign_key "consultations", "users"
+  add_foreign_key "diagnostics", "consultations"
+  add_foreign_key "messages", "assignations"
+  add_foreign_key "messages", "users"
+  add_foreign_key "patients", "users"
 end

9a77a330fdd2ee4e5bd08ec584cd6069366e547c
Marc PLAGNES <m.plagnes@hotmail.fr>
2025-03-11 12:28:48 +0100

    Validations et associations des models

--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -1,3 +1,10 @@
 class Diagnostic < ApplicationRecord
   belongs_to :consultation
+  has_many :assignations, dependent: :destroy
+
+  validates :description, presence: true
+  validates :speciality, presence: true
+
+  validates :selected, inclusion: { in: [true, false] }
+  validates :specialist_needed, inclusion: { in: [true, false] }
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :assignation
+
+  validates :content, presence: true
 end

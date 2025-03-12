84212779856105ba1f09a9640e2ac59f5ee71aad
Erwann Defoy <ecdefoy1@gmail.com>
2025-03-12 17:46:49 +0100

    first take on controller

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -1,9 +1,31 @@
 class DiagnosticsController < ApplicationController
+  before_action :set_consultation, only: %i[create]
+
   def index; end
 
   def show; end
 
-  def new; end
+  def new
+    @diagnostic = Diagnostic.new
+    if params[:consultation_id]
+      @consultation = Consultation.find(params[:consultation_id])
+    else
+      @consultations = Consultation.where(user: current_user).order(:starts_at)
+    end
+  end
 
-  def create; end
+  def create
+    @diagnostic = Diagnostic.new(diagnostic_params)
+    @diagnostic.consultation = @consultation
+  end
+
+  private
+
+  def set_consultation
+    @consultation = Consultation.find(params[:consultation_id])
+  end
+
+  def diagnostic_params
+    params.require(:diagnostic).permit(:desciption, :specialty)
+  end
 end

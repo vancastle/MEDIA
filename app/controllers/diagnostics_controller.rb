ea9501c83c3efe9ff43bf344f01f5337f4c713c9
Erwann Defoy <ecdefoy1@gmail.com>
2025-03-13 15:47:50 +0100

    openai

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -1,7 +1,9 @@
 class DiagnosticsController < ApplicationController
   before_action :set_consultation, only: %i[create]
 
-  def index; end
+  def index
+    @diagnostics = Diagnostic.where(consultation_id: params[:consultation_id])
+  end
 
   def show; end
 
@@ -15,8 +17,19 @@ class DiagnosticsController < ApplicationController
   end
 
   def create
-    @diagnostic = Diagnostic.new(diagnostic_params)
-    @diagnostic.consultation = @consultation
+    @consultation.update(consultation_params)
+
+    choices(@consultation).each do |choice|
+      @diagnostic = Diagnostic.new(
+        specialty: "Boisson gazeuse",
+        description: choice["message"]["content"]
+      )
+      @diagnostic.consultation = @consultation
+
+      render :new, status: :unprocessable_entity unless @diagnostic.save
+    end
+
+    redirect_to consultation_diagnostics_path(@consultation)
   end
 
   private
@@ -25,7 +38,23 @@ class DiagnosticsController < ApplicationController
     @consultation = Consultation.find(params[:consultation_id])
   end
 
-  def diagnostic_params
-    params.require(:diagnostic).permit(:desciption, :specialty)
+  def consultation_params
+    params.require(:consultation).permit(:prompt)
+  end
+
+  def choices(consultation)
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: [{
+          role: "user",
+          content: consultation.prompt
+        }],
+        n: 3
+      }
+    )
+
+    chatgpt_response["choices"]
   end
 end

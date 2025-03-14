e45aeedf1bcd601347a86eb541f0f5f3c9122e01
Erwann Defoy <ecdefoy1@gmail.com>
2025-03-14 14:24:13 +0100

    specialties are now in french

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  SPECIALTIES = %w[generaliste neurologue urologue dermatologue]
+  SPECIALTIES = %w[généraliste neurologue urologue dermatologue]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
